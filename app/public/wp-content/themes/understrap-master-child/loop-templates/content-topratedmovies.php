739ea71d3973c26978f906b2271461a733778174
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-03-13 14:58:17 +0100

    Removed top movies query and moved it to a template. Added css

---
+++ app/public/wp-content/themes/understrap-master-child/loop-templates/content-topratedmovies.php
@@ -0,0 +1,32 @@
+<?php
+
+$args = [
+  'post_type' 		 => 'movie',
+  'posts_per_page' => 5,
+  'meta_key' => '_kksr_ratings',
+  'orderby' => 'meta_value_num',
+  'order' => 'DESC'
+];
+$topRatedQuery = new WP_Query( $args );
+?>
+<h1>Top 10 rated movies</h1>
+<div class="top-rated-movies">
+<?php
+if ($topRatedQuery -> have_posts() ) :
+  $iteration = 0;
+  while ($topRatedQuery -> have_posts() ) : $topRatedQuery -> the_post();
+  $iteration++; ?>
+    <div class="movie">
+      <h2><?php echo $iteration . '.'; ?></h2>
+      <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="Movie image">
+      <a href="<?php echo get_post_permalink(); ?>">
+        <h4><?php echo get_the_title(); ?></h4>
+      </a>
+      <p>Rating: <?php echo get_post_meta(get_the_ID(), '_kksr_ratings', true) ?></p>
+    </div>
+    <?php
+  endwhile;
+  wp_reset_postdata();
+endif;
+?>
+</div>

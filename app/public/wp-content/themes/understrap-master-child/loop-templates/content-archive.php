f339860a3ed51f2b5d025cad80d988d9dc0ffe05
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-03-17 13:47:04 +0100

    Added archive.php and content-archive.php

---
+++ app/public/wp-content/themes/understrap-master-child/loop-templates/content-archive.php
@@ -0,0 +1,38 @@
+<?php
+
+$args = [
+  'post_type' 		 => 'movie',
+  'posts_per_page' => -1,
+  'orderby'        => 'title',
+  'order'          => 'ASC'
+];
+$topRatedQuery = new WP_Query( $args );
+?>
+<h1>All movies (A-Z)</h1>
+<div class="movies">
+<?php
+if ($topRatedQuery -> have_posts() ) :
+  while ($topRatedQuery -> have_posts() ) : $topRatedQuery -> the_post();
+  $rating = round(get_post_meta(get_the_ID(), '_kksr_avg', true), 1); ?>
+    <div class="movie-box">
+      <img class="movie-image" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="Movie image">
+      <a href="<?php echo get_post_permalink(); ?>">
+        <h4 class="movie-link"><?php echo get_the_title(); ?></h4>
+      </a>
+      <?php if (!empty(get_post_meta(get_the_ID(), '_imdb_id', true))) : ?>
+        <div class="imdb-icon"></div>
+      <?php endif; ?>
+      <p>Rating:
+      <?php
+      $ratedOrNot = empty($rating) ? 'No rating yet' : $rating;
+      echo $ratedOrNot;
+      ?>
+      </p>
+    </div>
+    <?php
+  endwhile;
+  wp_reset_postdata();
+  else : ?>
+    <?php get_template_part( 'loop-templates/content', 'none' ); ?>
+<?php endif; ?>
+</div>

a49ac295adc2c4f9c0e1b78e42490db17bddbb02
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-03-17 16:47:40 +0100

    Added pagination for all movies to homepage.

--- app/public/wp-content/themes/understrap-master-child/functions.php
+++ app/public/wp-content/themes/understrap-master-child/functions.php
@@ -1,8 +1,8 @@
 <?php
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-function my_theme_enqueue_styles() {
-
-    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+function my_theme_enqueue_styles()
+{
+    $parent_style = 'parent-style';
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
@@ -11,3 +11,14 @@ function my_theme_enqueue_styles() {
         wp_get_theme()->get('Version')
     );
 }
+function homepage_main_query( $query ) {
+  if (!is_admin() && $query->is_main_query()){
+    if($query->is_home()){
+      $query->set('posts_per_page', 10);
+      $query->set('post_type', 'movie');
+      $query->set('orderby', 'title');
+      $query->set('order', 'ASC');
+    }
+  }
+}
+add_action( 'pre_get_posts', 'homepage_main_query' );
--- app/public/wp-content/themes/understrap-master-child/loop-templates/content-movies.php
+++ app/public/wp-content/themes/understrap-master-child/loop-templates/content-movies.php
@@ -1,25 +1,18 @@
 <?php
 
-$args = [
-  'post_type' 		 => 'movie',
-  'posts_per_page' => -1,
-  'orderby'        => 'title',
-  'order'          => 'ASC'
-];
-$moviesQuery = new WP_Query( $args );
 ?>
 <h1>All movies (A-Z)</h1>
 <div class="movies">
 <?php
-if ($moviesQuery -> have_posts() ) :
-  while ($moviesQuery -> have_posts() ) : $moviesQuery -> the_post();
+if ( have_posts() ) :
+  while ( have_posts() ) :  the_post();
   $rating = round(get_post_meta(get_the_ID(), '_kksr_avg', true), 1); ?>
     <div class="movie-box">
       <img class="movie-image" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="Movie image">
       <a href="<?php echo get_post_permalink(); ?>">
         <h4 class="movie-link"><?php echo get_the_title(); ?></h4>
       </a>
-      <?php if (!empty(get_post_meta(get_the_ID(), '_imdb_id', true))) : ?>
+      <?php if ( !empty( get_post_meta( get_the_ID(), '_imdb_id', true ) ) ) : ?>
         <div class="imdb-icon"></div>
       <?php endif; ?>
       <p>Rating:
